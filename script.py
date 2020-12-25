490b4f5156f4a90b2d2a984e43518734800f7e97
laurindominik <67796250+laurindominik@users.noreply.github.com>
2020-12-25 22:04:50 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,22 @@
+# dependencies
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('IronPython.Wpf')
+
+# find the path of ui.xaml
+from pyrevit import UI
+from pyrevit import script
+xamlfile = script.get_bundle_file('ui.xaml')
+
+# import WPF creator and base Window
+import wpf
+from System import Windows
+
+class MyWindow(Windows.Window):
+    def __init__(self):
+        wpf.LoadComponent(self, xamlfile)
+
+
+
+# let's show the window (modal)
+MyWindow().ShowDialog()
